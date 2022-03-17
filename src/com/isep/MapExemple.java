b31165ae32542dd83d249a99077ccc3491c91c71
Paul Flammarion <pa.flammarion@gmail.com>
2022-03-17 09:47:50 +0200

    more about hash

--- src/com/isep/MapExemple.java
+++ src/com/isep/MapExemple.java
@@ -6,6 +6,9 @@ public class MapExemple {
     public static void main(String[] args){
         Map<String, String> map = new HashMap<String, String>();
 
+        //<Key, value> without key we can't find back the value, because
+        //the value is encrypted by the key
+
         map.put("Blue", "Joe’s least favourite");
         map.put("Green", "Mark’s favourite");
 
@@ -13,6 +16,7 @@ public class MapExemple {
         System.out.println(value + "\n");
 
 ////////Separate all data and print it
+        //You can use it to get back a key but.... not loose it plz
 
         for(Map.Entry<String, String> entry: map.entrySet()) {
             System.out.println(entry.getKey());
